e29e9d3d718344783afebd66c28f843d6b926c68
erdgeist <erdgeist>
2008-12-06 16:57:10 +0000

    Format for full scrapes was not parsed correctly.

--- ot_http.c
+++ ot_http.c
@@ -239,6 +239,7 @@ static ssize_t http_handle_stats( const int64 client_socket, char *data, char *d
             HTTPERROR_400_PARAM;
           break;
       }
+      break;
     case 6:
       if( byte_diff(data,6,"format")) {
         scan_urlencoded_skipvalue( &c );
@@ -562,4 +563,4 @@ ssize_t http_handle_request( const int64 client_socket, char *data, size_t recv_
   return reply_size;
 }
 
-const char *g_version_http_c = "$Source: /home/cvsroot/opentracker/ot_http.c,v $: $Revision: 1.21 $\n";
+const char *g_version_http_c = "$Source: /home/cvsroot/opentracker/ot_http.c,v $: $Revision: 1.22 $\n";
